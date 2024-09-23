1c6520adde74c8a1be270a90f8c87711a619536b
Rahmath Irshad <83600648+Rahmathirshad@users.noreply.github.com>
2024-09-23 14:54:11 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,7 +11,7 @@ const Experience = () => {
             <p className="text-4xl font-bold inline border-b-4 border-gray-500">
               Work Experience
             </p>
-            <p className="py-6">I have total 1.3+ years of hands on development experience</p>
+            <p className="py-6">I have total 1.5 years of hands on development experience</p>
           </div>
           <div class="timeline">
             <div class="timeline-empty"></div>
